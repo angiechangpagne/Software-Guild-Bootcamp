f8a2a059993d0a6abfd11aaf1229846352568bb5
Angie Changpagne <43552993+angiechangpagne@users.noreply.github.com>
2019-06-09 11:31:18 -0400

    Add files via upload

---
+++ OOP Mastery/src/main/java/com/sg/floormastery/service/ServiceProductionException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.floormastery.service;
+
+public class ServiceProductionException extends Exception{
+    
+    public ServiceProductionException(String message){
+        super(message);
+    }
+    
+    public ServiceProductionException(String message, Throwable cause){
+        super(message, cause);
+    }
+    
+}
